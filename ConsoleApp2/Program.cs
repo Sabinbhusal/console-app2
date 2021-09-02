dc0d7508d119ee758a34e6790890bc0f4249d5c2
sabinbhusal <sabin_bhusal@dialog.com.au>
2021-09-02 15:27:41 +0930

    programming is done

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //int a = 1;
+            //int b;
+            //while (a < 5)
+            //{
+            //    Console.WriteLine("enter number");
+            //    b = Convert.ToInt32(Console.ReadLine());
+            //    if (b < 0)
+            //        break;
+            //    a = a + 1;
+            //}
+
+            //Console.ReadLine();
+            int a = 1;
+            while (a < 10)
+            {
+                Console.WriteLine(a);
+                a = a + 1;
+            }
+            Math.Min(5, 10);
+            Console.ReadLine();
+        }
+    }
+}
